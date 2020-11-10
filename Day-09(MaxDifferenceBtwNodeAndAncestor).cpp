705ccfe080058f6b159e3c8e49bb4a452ef8ef9c
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-10 23:26:54 +0530

    Update Day-09(MaxDifferenceBtwNodeAndAncestor).cpp

--- Day-09(MaxDifferenceBtwNodeAndAncestor).cpp
+++ Day-09(MaxDifferenceBtwNodeAndAncestor).cpp
@@ -3,3 +3,36 @@ Time complexity :
 Space complexity :
 My approach :
 */
+
+class Solution {
+
+public:
+
+int helper(TreeNode* root,int curmin,int curmax)
+{
+    if(root==NULL)
+    {
+        return curmax-curmin;
+    }
+    
+    curmax=max(root->val,curmax);
+    curmin=min(root->val,curmin);
+    
+    int l=helper(root->left,curmin,curmax);
+    int r=helper(root->right,curmin,curmax);
+    
+    return max(l,r);
+}
+
+int maxAncestorDiff(TreeNode* root) {
+    
+    if(root==NULL)
+    {
+        return 0;
+    }
+    
+    return helper(root,root->val,root->val);
+    
+    
+}
+};
