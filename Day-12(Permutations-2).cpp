de31c2e3f94781d5c3cc26dce75fcb17c3248203
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-13 11:16:12 +0530

    Update Day-12(Permutations-2).cpp

--- Day-12(Permutations-2).cpp
+++ Day-12(Permutations-2).cpp
@@ -41,3 +41,44 @@ public:
         return result;
     }
 };
+
+//Just a variation of code , but clears a lot of things such as the re-swap can be done in backtracking part.
+
+class Solution {
+public:
+    
+    void helper(vector<int> nums,set<vector<int>>& res,int l ,int r)
+    {
+        if(l == r)
+        {
+            res.insert(nums);
+            return;
+        }
+        else
+        {
+            for(int i=l;i<=r;i++)
+            {
+                swap(nums[i],nums[l]);
+                helper(nums,res,l+1,r);
+//                 swap(nums[i],nums[l]);
+            }
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums)
+    {
+        int n = nums.size();
+         set<vector<int>> res;
+        vector<vector<int>> result;
+        if(n==0) return result;
+        
+        helper(nums,res,0,n-1);
+        
+        for(auto it : res)
+        {
+            result.push_back(it);
+        }
+        return result;
+    }
+};
+
