737f660e2f6baa2950fff1219bf0c6f3904a195a
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-13 11:37:43 +0530

    Update Day-12(Permutations-2).cpp

--- Day-12(Permutations-2).cpp
+++ Day-12(Permutations-2).cpp
@@ -82,3 +82,26 @@ public:
     }
 };
 
+// Method -2 ,Pruning the Unrequired branches caused by the duplicates in the given nums vector
+// This is a more efficient method.
+class Solution {
+public:
+    void recursion(vector<int> num, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(num);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && num[i] == num[k]) continue;
+            swap(num[i], num[k]);
+            recursion(num, i+1, j, res);
+        }
+    }
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        sort(num.begin(), num.end());
+        vector<vector<int> >res;
+        recursion(num, 0, num.size(), res);
+        return res;
+    }
+};
+
