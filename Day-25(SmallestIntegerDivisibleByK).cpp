574ccdf6d2d61b2fbb5f743d7417c913cc55cac7
Ayush Chaudhary <46848081+acxdme@users.noreply.github.com>
2020-11-26 12:30:30 +0530

    Create Day-25(SmallestIntegerDivisibleByK).cpp

---
+++ Day-25(SmallestIntegerDivisibleByK).cpp
@@ -0,0 +1,15 @@
+/*
+Time complexity :
+Space complexity :
+My approach :
+*/
+
+class Solution {
+public:
+  int smallestRepunitDivByK(int K) {
+        for (int r = 0, N = 1; N <= K; ++N)
+            if ((r = (r * 10 + 1) % K) == 0)
+                return N;
+        return -1;
+    }
+};
